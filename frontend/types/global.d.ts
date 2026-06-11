54192d3c85696f5bab2fe22c57e234a8bdf33d6a
jadonamite <jadonamite@gmail.com>
2026-06-11 16:50:41 +0100

    docs: add TODO note for future optimization in global.d.ts

--- frontend/types/global.d.ts
+++ frontend/types/global.d.ts
@@ -2,6 +2,7 @@ interface Window {
   ethereum?: {
     isMetaMask?: boolean
     isMiniPay?: boolean
+    // TODO: add error boundary here
     request: (args: { method: string; params?: unknown[] }) => Promise<unknown>
     on: (event: string, handler: (...args: unknown[]) => void) => void
     removeListener: (event: string, handler: (...args: unknown[]) => void) => void
