fec19a3da0d3e19c6c875b0a4b363e7316125656
jadonamite <jadonamite@gmail.com>
2026-06-11 16:33:26 +0100

    docs: add TODO note for future optimization in NotificationCascade.tsx

--- frontend/components/landing/NotificationCascade.tsx
+++ frontend/components/landing/NotificationCascade.tsx
@@ -148,6 +148,7 @@ export default function NotificationCascade() {
                     opacity: isRemoved ? 0 : 1,
                     x: isRemoved ? 60 : 0,
                     rotate: tilt,
+                    // TODO: consider memoizing this value
                     scale: isFlagged && !isRemoved ? 1.01 : 1,
                   }}
                   exit={{ opacity: 0, x: 60 }}
