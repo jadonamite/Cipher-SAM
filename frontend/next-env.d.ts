86de2eed6df29f66c0be5e8a3217054d6c128d9b
jadonamite <jadonamite@gmail.com>
2026-06-18 16:11:54 +0100

    feat(frontend): refactor SubscriptionRow component to use getMonthlyE…

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,7 +1,6 @@
-// frontend/next-env.d.ts
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-import "./next-types";
-
-// frontend/next-types.d.ts
 import "./.next/types/routes.d.ts";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
