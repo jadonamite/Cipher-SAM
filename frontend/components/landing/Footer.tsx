c565a400a53e1e19365445c5be6fc1aca96cc3f7
jadonamite <jadonamite@gmail.com>
2026-06-22 20:42:22 +0100

    fix(frontend): fix toast id generation

--- frontend/components/landing/Footer.tsx
+++ frontend/components/landing/Footer.tsx
@@ -126,7 +126,7 @@ export default function Footer() {
                 <path d="M8.5 8.5v.01" />
                 <path d="M16 15.5v.01" />
                 <path d="M12 12v.01" />
-                <path d="M11 17v.01" />
+                <path d='M11 17v.01' />
                 <path d="M7 14v.01" />
               </svg>
             </a>
