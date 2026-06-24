21d2a47503b5d5b26cd0b3c92539db1651e73638
jadonamite <jadonamite@gmail.com>
2026-06-24 05:30:34 +0100

    fix(frontend): remove unused reference in next-env.d.ts

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,9 +1,6 @@
-// frontend/references.d.ts
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-
-// frontend/next-env.d.ts
-import "./references.d.ts";
 import "./.next/types/routes.d.ts";
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
