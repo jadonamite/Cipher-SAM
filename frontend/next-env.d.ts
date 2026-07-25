713b0927c95fdddab590484465698587c9abd79b
jadonamite <jadonamite@gmail.com>
2026-07-25 17:53:29 +0100

    refactor(frontend): refactor next-env.d

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+// NOTE: revisit this logic after API migration
 import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
